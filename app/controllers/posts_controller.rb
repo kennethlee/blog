e96d4cdcc3f07d171ceb8797c9dc7ef29a5dca35
Kenneth Lee <kennyxlee@gmail.com>
2015-11-11 16:33:25 -0800

    PostsController: define private method set_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,16 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
+
+  private
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
