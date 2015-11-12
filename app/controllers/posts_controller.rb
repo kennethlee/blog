27311905c5c33762844f000ea9916ca37ddf6eaf
Kenneth Lee <kennyxlee@gmail.com>
2015-11-11 18:27:23 -0800

    posts#edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit]
 
   def index
     @posts = Post.all
@@ -10,6 +10,9 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+  end
+
+  def edit
   end
 
   private
