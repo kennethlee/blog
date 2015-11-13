9bf82a1a56c0cd6f14834cf4874560ababc4d995
Kenneth Lee <kennyxlee@gmail.com>
2015-11-13 00:12:48 -0800

    PostController destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -24,6 +24,11 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
     redirect_to @post
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
