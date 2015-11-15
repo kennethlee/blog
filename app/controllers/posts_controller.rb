6ae7582a3cf27edccc134cf759d522e431d4c156
Kenneth Lee <kennyxlee@gmail.com>
2015-11-14 18:50:05 -0800

    admin login req for: new, edit, create, update, destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_admin!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
